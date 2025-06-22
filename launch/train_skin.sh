fd71d15159517d5ad1890d4b3fac37291e4768ab
WilmaLiu2005 <17701248665@189.cn>
2025-06-22 11:14:44 +0800

    liuxinyu improvement1

---
+++ launch/train_skin.sh
@@ -0,0 +1,1 @@
+python train_skin.py --train_data_list data/train_list.txt --val_data_list data/val_list.txt --data_root data --model_name pct --output_dir output/skin --batch_size 16 --epochs 1000 --learning_rate 0.0001
